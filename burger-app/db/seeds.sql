53f777553af6ae3aa88380fc3fe3b1eaebe9a3de
Dawn Sallas <dawndesignerd@Dawns-MBP-2.hsd1.va.comcast.net>
2019-10-07 17:09:26 -0400

    I have added the basics to allow the app to run and add burgers, having trouble devouring the burgers.

--- burger-app/db/seeds.sql
+++ burger-app/db/seeds.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO burgers (burger_name, devoured)
-VALUE ("Slap My Grandma", false), ("SunRise", false), ("Damn That's Hot", false);
+VALUE ("Slap My Grandma", 0), ("SunRise", 0), ("Damn That's Hot", 0);
